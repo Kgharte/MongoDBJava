5fafb5b966bfc89fd1563961ef590681e90d5d4c
Kgharte <ghartekalpesh10@gmail.com>
2019-08-26 00:27:33 +0530

    final commmit

--- src/main/java/MongoDBClient.java
+++ src/main/java/MongoDBClient.java
@@ -1,5 +1,45 @@
+import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Set;
 
 public class MongoDBClient {
+    public static void main(String[] args) {
+        MongoClient mongoClient = new MongoClient(
+                Arrays.asList(new ServerAddress("localhost", 27017)));
+        MongoDatabase db = mongoClient.getDatabase("local");
+        MongoCollection<Document> coll = db.getCollection("catalog");
+                Document catalog = new Document("journal", "Oracle Magazine")
+                        .append("publisher", "Oracle Publishing")
+                        .append("edition", "November December 2013")
+                        .append("title", "Engineering as a Service")
+                        .append("author", "David A. Kelly");
+        coll.insertOne(catalog);
+
+        catalog = new Document("journal", "Oracle Magazine")
+                .append("publisher", "Oracle Publishing")
+                .append("edition", "November December 2013")
+                .append("title", "Quintessential and Collaborative")
+                .append("author", "Tom Haunert");
+        coll.insertOne(catalog);
+        FindIterable<Document> iterable = coll.find();
+        String documentKey = null;
+        for (Document document : iterable) {
+            Set<String> keySet = document.keySet();
+            Iterator<String> iter = keySet.iterator();
+            while (iter.hasNext()) {
+                documentKey = iter.next();
+                System.out.println(documentKey);
+                System.out.println(document.get(documentKey));
+            }
+        }
+        mongoClient.close();
+    }
 
 }
